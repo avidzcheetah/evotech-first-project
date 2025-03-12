acb0e17411763af5e85c47f3c03ee29cf489fc2e
unknown <avidu@protonmail.com>
2025-03-12 09:36:57 +0530

    Create movies GET endpoint

--- app/api/v1/movies/route.js
+++ app/api/v1/movies/route.js
@@ -1,23 +1,14 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/mongodb";
 
-export const GET = async (req) => {
-  // Get Movies from the MongoDB
-  try {
-    // fetch movies from the database
-    const movies = await db
-      .collection("movies_n")
-      .find({})
-      .sort({ metacritic: -1 })
-      .limit(10)
-      .toArray();
+const MOVIES = [
+  { id: 1, title: "Harry Potter" },
+  { id: 2, title: "Dr. Doolittle" },
+  { id: 3, title: "Intesteller" },
+  { id: 4, title: "Awaken" },
+  { id: 5, title: "The Martian" },
+];
 
-    return NextResponse.json(movies);
-  } catch (error) {
-    console.log("MONGODB ERROR", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
-  }
+export const GET = async (req) => {
+  return NextResponse.json({ success: true, movies: MOVIES });
 };
